5dc693ecc594bed046a09acd24b3ccd97f1e0b05
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-08-17 10:15:41 +0300

    feat: make initial base for epics

--- FakeNFT/Flows/Catalog/Data/CatalogServiceProvider.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-final class CatalogServiceProvider {
-	private let session = URLSession(configuration: .default)
-	lazy var apiClient: APIClient = {
-		APIClient(session: session)
-	}()
-}
