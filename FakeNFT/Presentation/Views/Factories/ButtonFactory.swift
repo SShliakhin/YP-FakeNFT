b6c027e1bf769b5c49624c4d762ca70c3fb7c3bb
Shliakhin Sergei <sshlyahin@yahoo.com>
2023-09-18 15:09:32 +0300

    refactor: add some small changes

--- FakeNFT/Presentation/Views/Factories/ButtonFactory.swift
+++ FakeNFT/Presentation/Views/Factories/ButtonFactory.swift
@@ -4,10 +4,9 @@ enum ButtonFactory {
 	static func makeButton(
 		image: UIImage,
 		tintColor: UIColor = Theme.color(usage: .black),
-		type: UIButton.ButtonType = .custom,
 		event: (() -> Void)? = nil
 	) -> UIButton {
-		let button = UIButton(type: type)
+		let button = UIButton(type: .custom)
 		button.setImage(image, for: .normal)
 		button.tintColor = tintColor
 		button.event = event
@@ -16,19 +15,20 @@ enum ButtonFactory {
 	}
 
 	static func makeLabelButton(
-		label: UILabel,
+		label: UILabel? = nil,
 		backgroundColor: UIColor = .clear,
 		cornerRadius: CGFloat = .zero,
-		type: UIButton.ButtonType = .custom,
 		event: (() -> Void)? = nil
 	) -> UIButton {
-		let button = UIButton(type: type)
+		let button = UIButton(type: .custom)
 
-		button.setTitle(label.text, for: .normal)
-		button.setTitleColor(label.textColor, for: .normal)
-		button.titleLabel?.font = label.font
-		button.titleLabel?.numberOfLines = label.numberOfLines
-		button.titleLabel?.textAlignment = label.textAlignment
+		if let label = label {
+			button.setTitle(label.text, for: .normal)
+			button.setTitleColor(label.textColor, for: .normal)
+			button.titleLabel?.font = label.font
+			button.titleLabel?.numberOfLines = label.numberOfLines
+			button.titleLabel?.textAlignment = label.textAlignment
+		}
 
 		button.backgroundColor = backgroundColor
 		button.layer.cornerRadius = cornerRadius
